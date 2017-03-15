d6496846aa8c8fb630bede45cfbea8a5a5a570a8
HellzYeahh <alexmerna_am@yahoo.com>
2017-03-15 17:14:36 -0400

    Someshit

--- app/src/main/java/com/xyz/activityswitcher/FirstActivity.java
+++ app/src/main/java/com/xyz/activityswitcher/FirstActivity.java
@@ -1,13 +1,55 @@
 package com.xyz.activityswitcher;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends Activity {
+
+    private Button BTNFirstAcivity;
+    private TextView lblDatafromSecondActivity;
+    private EditText txtPassed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+
+        lblDatafromSecondActivity = (TextView) findViewById(R.id.DatafromSecondActivity);
+        txtPassed = (EditText) findViewById(R.id.editTextFirst);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String thestring = extras.getString("data");
+
+            if (thestring.isEmpty()){
+                lblDatafromSecondActivity.setText("Nothing Provided");
+
+            }
+
+            else
+            {
+                lblDatafromSecondActivity.setText(thestring);
+            }
+
+        }
+        BTNFirstAcivity = (Button) findViewById(R.id.BTNFirstActivity);
+        BTNFirstAcivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String passedString = txtPassed.getText().toString();
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("data", passedString);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/xyz/activityswitcher/SecondActivity.java
+++ app/src/main/java/com/xyz/activityswitcher/SecondActivity.java
@@ -1,13 +1,55 @@
 package com.xyz.activityswitcher;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends Activity {
+
+    private Button btnSecondButton;
+    private TextView lblDatafromfirstActivity;
+    private EditText txtPassed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+        lblDatafromfirstActivity = (TextView) findViewById(R.id.DatafromFirstActivity);
+        txtPassed = (EditText) findViewById(R.id.editTextSecond);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String thestring = extras.getString("data");
+
+            if (thestring.isEmpty()){
+                lblDatafromfirstActivity.setText("Nothing Provided");
+
+            }
+
+            else
+            {
+                lblDatafromfirstActivity.setText(thestring);
+            }
+
+        }
+        btnSecondButton = (Button) findViewById(R.id.BTNFirstActivity);
+        btnSecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String passedString = txtPassed.getText().toString();
+                Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+                intent.putExtra("data", passedString);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
